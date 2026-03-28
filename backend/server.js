6103910090e967afe3ea2c3d9d5c572290d4f125
vpandya108 <vpandya108@rku.ac.in>
2026-03-28 21:48:22 +0530

    test api using thunderclient

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes"); // 👈 ADD THIS
+
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -13,12 +14,14 @@ mongoose.connect("mongodb://127.0.0.1:27017/Skill-Swap-react")
 .then(() => console.log("MongoDB Connected ✅"))
 .catch(err => console.log(err));
 
+// 👇 ADD THIS LINE
+app.use("/api/users", userRoutes);
+
 // test route
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-// start server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
