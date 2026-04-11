ca5cb875c1d33922889b793a650e889a32bbbc73
vpandya108 <vpandya108@rku.ac.in>
2026-04-11 19:06:13 +0530

    Add message routes to server for handling API requests

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+require("dotenv").config(); 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path=require("path")
 const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/MessageRoute");
+
 
 const app = express();
 
@@ -10,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/messages", messageRoutes);
 
 // MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/Skill-Swap-react")
